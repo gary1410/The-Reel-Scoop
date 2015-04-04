ea1064b7a5058c1ac8eb0032a1cf1e787b65e118
Gary Tsai <gary.m.tsai@gmail.com>
2015-04-04 05:42:55 -0700

    User is able to sort through the movies view via ajax 	* User has the ablity to Sort and Paginate on Ajax 	* User has the ability to Search on Submit via Ajax 	* User has the ability to Search on Keyup and do a "live search"

--- app/assets/javascripts/scripts.js
+++ app/assets/javascripts/scripts.js
@@ -1,21 +1,20 @@
 $(document).ready(function(){
 
-// console.log('hi')
-// alert("hi")	
+	$("#movies th a, #movies .pagination a").on("click", function(){
+		$.getScript(this.href);
+		return false;
+	});
 
-// // $(function(){
-// // 	$('#a-link').remove();   
-	
-// // 	$('<img alt="" />').attr('id', 'loader').attr('src', 'ajax-loader.gif').appendTo('#image-container');
-	
-// 	//assign your api key equal to a variable
-// 	var apiKey = '[ROTTEN_API_KEY]';
-	
-// 	//the initial json request to flickr
-// 	//to get your latest public photos, use this request: http://api.flickr.com/services/rest/?&amp;method=flickr.people.getPublicPhotos&amp;api_key=' + apiKey + '&amp;user_id=29096781@N02&amp;per_page=15&amp;page=2&amp;format=json&amp;jsoncallback=?
-// 	// $.getJSON('http://api.flickr.com/services/rest/?&amp;method=flickr.photosets.getPhotos&amp;api_key=' + apiKey + '&amp;photoset_id=72157619415192530&amp;format=json&amp;jsoncallback=?',
-	
-// 	data = $.getJSON('http://api.rottentomatoes.com/api/public/v1.0.json?apikey=' + apiKey)
+	$('#movies_search').on("submit", function(){
+		$.get(this.action, $(this).seralize(), null, 'script')
+		return false;
+	})
 
-// 	console.log(data)
-})
+	$("#movies_search input").keyup(function() {
+		$.get($("#movie_search").attr("action"), $("#movies_search").serialize(), null, "script");
+		return false;
+	});
+
+
+});
+
